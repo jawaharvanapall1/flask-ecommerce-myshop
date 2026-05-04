4db8a24dca3031834bf757d2b84c62ecf6058c38
jawaharvanapall1 <jawaharvanapalli705@gmail.com>
2026-05-04 19:24:55 +0530

    fix db config

--- database/connection.py
+++ database/connection.py
@@ -3,9 +3,9 @@ import mysql.connector as SQLC
 # database configuration
 def databaseConfig():
     db_config = SQLC.connect(
-        host='localhost',
-        user = 'root',
-        password='root',
-        database = 'ecommerce1'
+    host='localhost',
+    user='jawahar',
+    password='1234',
+    database='ecommerce1'
     )
     return db_config
